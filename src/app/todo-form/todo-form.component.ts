6c51a33270505650145ff9d4f364a7f913681384
TuoAdama <tuoadama17@gmail.com>
2024-09-07 17:34:09 +0200

    feat: create form

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {Todo} from "../../models/Todo";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css'
 })
 export class TodoFormComponent {
+  form: FormGroup = new FormGroup({
+    title: new FormControl<String>('', [Validators.required, Validators.minLength(2)]),
+    status: new FormControl<String>('', [Validators.required]),
+    description: new FormControl<String>('', [Validators.required, Validators.minLength(3)]),
+  })
 
+  onSubmit(){
+    let todo: Todo = this.form.value as Todo
+  }
 }
